54b887d578b73208bbf876dbe03c8c8a3d105f3f
Daniel <daniel.rosenbaum.il@gmail.com>
2021-05-04 14:16:07 +0300

    route Mail and note apps

--- App.jsx
+++ App.jsx
@@ -1,6 +1,9 @@
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
 import { Home } from './pages/Home.jsx'
+import { MailApp } from './apps/mail/MailApp.jsx'
+import { NoteApp } from './apps/note/NoteApp.jsx'
+
 
 // Simple React Component
 export function App() {
@@ -8,13 +11,13 @@ export function App() {
         <Router>
             {/* <Header/> */}
             <main>
-                {/* <Switch> */}
-                {/* <Route component={NoteApp} path="/note" />
-                <Route component={MailApp} path="/mail/" />
-                <Route component={BookApp} path="/book" />
-            <Route component={AboutUs} path="/about" /> */}
+                <Switch>
+                <Route component={NoteApp} path="/note" />
+                <Route component={MailApp} path="/mail" />
+                {/* <Route component={BookApp} path="/book" /> */}
+                {/* <Route component={AboutUs} path="/about" /> */}
                 <Route component={Home} path="/" />
-                {/* </Switch> */}
+                </Switch>
             </main>
             {/* <Footer/> */}
         </Router>
---
+++ apps/mail/MailApp.jsx
@@ -0,0 +1,12 @@
+export class MailApp extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+
+            <h1>MAIL</h1>
+
+        )
+    }
+}
