96efa6cf19cb4ebec5626a2eee3ac8a75a3c0b92
Daniel <daniel.rosenbaum.il@gmail.com>
2021-05-04 14:40:50 +0300

    git fix

--- apps/mail/MailApp.jsx
+++ apps/mail/MailApp.jsx
@@ -1,30 +1,13 @@
-import {UserPreview} from './cmps/user-preview.jsx'
-import {Home} from './pages/home.jsx'
+export class MailApp extends React.Component {
+    state = {
 
-// Simple React Component
-export function App() {
-    return <section className="app">
-       <h1>My App</h1>
-       <UserPreview/>
-       <Home/>
-    </section>
+    }
+
+    render() {
+        return (
+
+            <h1>NOTE</h1>
+
+        )
+    }
 }
-
-
-
-// Using Class:
-// export class App extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 <header>
-//                     <h1>Lets Play</h1>
-//                 </header>
-//                 <main>
-//                     <Home />
-//                 </main>
-//             </div>
-//         )
-//     }
-// }
-
