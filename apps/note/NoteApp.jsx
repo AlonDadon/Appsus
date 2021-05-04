759e545d8395e1455ed8da5b3f41a18f419adc3c
ada1milion <77745921+ada1milion@users.noreply.github.com>
2021-05-04 13:01:04 +0300

    add base

---
+++ apps/note/NoteApp.jsx
@@ -0,0 +1,11 @@
+import {Home} from './pages/Home.jsx'
+
+// Simple React Component
+export function App() {
+    return <section className="app">
+       <h1>My App</h1>
+       <UserPreview/>
+       <Home/>
+    </section>
+}
+
